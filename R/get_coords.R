bbaf944366a1d36eb59031df8040de575a078f9c
Alim Dayim <alimdayim@hotmail.com>
2021-11-04 10:45:52 +0000

    Update get_coords for more infor

--- R/get_coords.R
+++ R/get_coords.R
@@ -1,18 +1,22 @@
-#' Get the coordinates of the box object
+#' Get the coordinates of the textbox object
 #' 
-#' This function will get the coordinates of the box object.
+#' This function will get the coordinates of the textbox object.
 #'
-#' @param x A box object
+#' @param x A textbox object
 #'
 #' @return A list of coordinates will return:
 #' \item{left}{Left (x-min) side coordinate.}
 #' \item{right}{Right (x-max) side coordinate.}
 #' \item{bottom}{Bottom (y-min) side coordinate.}
 #' \item{top}{Top (y-max) side coordinate.}
+#' \item{top.mid }{Coordinate vector of top middle, measured by grob.}
+#' \item{left.mid}{Coordinate vector of left middle, measured by grob.}
+#' \item{bottom.mid}{Coordinate vector of bottom middle, measured by grob.}
+#' \item{right.mid}{Coordinate vector of right middle, measured by grob.}
 #' \item{x}{X (center x) coordinate.}
 #' \item{y}{Y (center y) coordinate.}
-#' \item{width}{Width of the box.}
-#' \item{height}{Height of the box.}
+#' \item{width}{Width of the textbox, derived with \code{grobWidth}.}
+#' \item{height}{Height of the textbox, derived with \code{grobHeight}.}
 #' \item{half_width}{Half width of the box.}
 #' \item{half_height}{Half height of the box.}
 #' 
@@ -23,33 +27,28 @@
 #' get_coords(fg)
 #' 
 get_coords <- function(x) {
-  if(!inherits(x, "box"))
-    stop("Object x must be box.")
-  
-  args <- attr(x, "args")
-  args <- args[c("x", "y", "width", "height")]
-  do.call(create_coords, args)
-}
+  if(!inherits(x, "textbox"))
+    stop("Object x must be textbox.")
 
-
-#' Convert position to coordinates
-#' @keywords internal
-create_coords <- function(x, y, width, height){
-  
-  width <- convertWidth(width, "mm", valueOnly = TRUE)
-  height <- convertHeight(height, "mm", valueOnly = TRUE)
+  width <- convertWidth(grobWidth(x), "mm", valueOnly = TRUE)
+  height <- convertHeight(grobHeight(x), "mm", valueOnly = TRUE)
   
   half_height <- unit(height/2, "mm")
   half_width <- unit(width/2, "mm")
   
-  list(left = x - half_width,
-       right = x + half_width,
-       bottom = y - half_height,
-       top = y + half_height,
-       x = x,
-       y = y,
-       width = width,
-       height = height, 
+  list(left = x$x - half_width,
+       right = x$x + half_width,
+       bottom = x$y - half_height,
+       top = x$y + half_height,
+	     top.mid    = unit.c(grobX(x, 90), grobY(x, 90)),
+       left.mid   = unit.c(grobX(x, 180), grobY(x, 180)),
+       bottom.mid = unit.c(grobX(x, 90), grobY(x, 270)),
+       right.mid  = unit.c(grobX(x, 0), grobY(x, 0)),
+       x = x$x,
+       y = x$y,
+       width  = width,
+       height = height,
        half_width = half_width,
        half_height = half_height)
 }
+
