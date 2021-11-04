6ca03522598618ca369a3853e2f9972757cc055a
Alim Dayim <alimdayim@hotmail.com>
2021-11-04 10:45:23 +0000

    Deperecated functions

---
+++ R/deprecated.R
@@ -0,0 +1,15 @@
+#' Build consort diagram
+#' 
+#' This function has been deprecated, no need to use this function.
+#' Please check the vignettes
+#' 
+#' @param consort_list A list of nodes.
+#' @param label_list A list of label nodes.
+#'
+#' @export
+#' 
+build_consort <- function(consort_list, label_list = NULL){
+  
+  warning("Function build_consort has deprecated, please check the vignettes")
+  
+}
