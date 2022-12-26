ec59334e56ec5783b6ab67a937df7423b552308d
Alimu Dayimu <alimdayim@hotmail.com>
2022-12-26 01:14:45 +0000

    Tests not working or slow

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,6 +4,7 @@ library(consort)
 test_check("consort")
 
 
+# vdiffr always fails on MAC and Ubuntu, don't know why
 to_grviz <- function(x) {
   path <- tempfile(fileext = ".gv")
   cat(x, file = path)
